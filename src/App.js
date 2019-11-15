4c8ff11be3903755cc8d69387f035d7fb923663d
Tae Wook Ha <taewookha@Taes-MacBook-Pro.local>
2019-11-14 22:07:21 -0800

    app text change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I am here
+         I was changed on the feature branch
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
